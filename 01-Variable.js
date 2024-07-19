4863d76193e86d75fa4ffa15f513df5496841b86
Arsh Mishra <161199109+ArshMishra1@users.noreply.github.com>
2024-07-19 13:14:44 +0530

    Add files via upload

---
+++ 01-Variable.js
@@ -0,0 +1,10 @@
+const accountId = 1234
+let accountEmail = "Arshmishra@gmail.com"
+var accountPassword = "8055"
+accountcity = "Jaipur"
+
+
+accountEmail = "mishraarsh@gmail.com"
+accountPassword = " 805524"
+accountcity = "Sultanpur"
+console.log([ accountEmail , accountPassword, accountcity]);
