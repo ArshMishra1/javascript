37f3e306e0a4b92f35c37d94b8e5390570d7d7d7
Arsh Mishra <161199109+ArshMishra1@users.noreply.github.com>
2024-07-21 08:59:51 +0530

    Add files via upload

---
+++ 03-Operators and expressions.js
@@ -0,0 +1,136 @@
+                                        // operator in js
+// arthmatice operator
+
+let a = 45;
+let b =  35;
+console.log("a + b = ", a+b); // addition operator
+console.log("a - b = ", a-b)  // subtraction operator
+console.log("a / b = ", a/b)  // devsion operator
+console.log("a * b = ", a*b)  // multiplicaton operator
+console.log("a ** b = ", a**b) //exponetional operator its mean power
+console.log("a % b = ", a%b) // modules operator
+console.log("a++  = ", a++) //increment operator
+console.log("a-- = ", a--) //Decrement operator
+
+ //question
+
+ console.log("++a = ", ++a)
+ console.log("a++  = ", a++) 
+ console.log("a-- = ", a--)
+ console.log("--a = ", --a)
+ console.log("a = " , a)
+ console.log("a-- = ", a--)
+ // Assignment Operators
+
+          // a = a +5  same ass 
+ a +=5
+ a -=5
+ a *=2
+ console.log(a);
+
+ //Camparison operators
+
+ let c1 = 45;
+ let c2 = 45;
+ console.log("c1 == c2", c1 == c2);
+ console.log("c1 !== c2", c1 !== c2);
+ console.log("c1 === c2", c1 === c2);
+ console.log("c1 > c2", c1 > c2);
+ console.log("c1 < c2", c1 < c2);    
+                                  //  Arithmetic Operator question 
+
+   //addition
+    let a = 20;                                               
+    let b = 2;
+    let c= a+b;
+    console.log(c);                      //1st type
+    console.log("a + b =", a+b)          // second type
+    console.log(a+b);                     // third type
+     
+    // subtraction
+     console.log( a-b);
+
+    //multiplication
+    console.log(a*b);
+
+    //divison
+    console.log(a/b);
+
+    //exponentiaton
+    console.log(a**b)
+
+    //increment
+    a++;
+    console.log(a+b);
+
+    //decrement
+    a--;
+    console.log(a+b);
+
+    //modulus(Remainder)
+    console.log(a%b);  
+                                  
+
+                                        //   assignment Operator question
+
+//addtion
+let a=10;
+let b=2;
+ a+=b;            //it's is same as a=a+b
+ console.log(a)
+ //substraction
+ a-=b;               //it' is same  as a=a-b
+ console.log(a);
+//multipliacton
+ a*=b;               //it' is same as a=a*b
+ console.log(a);
+//exponentiation        //it is same as a=a**b or (a)^2 where b=2
+ a**=b;
+ console.log(a);
+                                        
+                                      //camparison operator
+ //equal to
+ let a =2;
+ let b=2;
+ console.log(a==b);
+ //equal value or equal not type
+ console.log(a===b);
+ //not equal
+ console.log(a!=b);
+
+ //not equal or equal not type
+ console.log(a!==b);
+ //greater than
+ console.log(a>b);
+ //greater than or equal to
+ console.log(a>=b);
+ //less than
+ console.log(a<b);
+ //less than or equal to
+ console.log(a<=b);
+
+                                          //Logical Operator question
+
+//AND question true
+ let x;
+ x=(5==5) &&(6==6); //because both condition is true
+ console.log(x);
+
+//AND question flase
+let x;
+x=(5==6)&&(6==6); //because first condition is flase
+console.log(x);
+
+//OR question true
+x=(5==5)||(6==7);// because first condition is true
+console.log(x);
+
+//OR question flase
+x=(5==6)||(5==7);        //because both condition is flase
+console.log(x);
+
+//NOT question 
+x=!((5==6)||(5==6))              //invert flase into true
+console.log(x);
+
+
