9a5f6a6597ddb00762380f6db577dd46cdebd12f
Arsh Mishra <161199109+ArshMishra1@users.noreply.github.com>
2024-07-23 13:08:33 +0530

    Add files via upload

---
+++ OperatorPrecendence.js
@@ -0,0 +1,19 @@
+         // operator precendence
+// the genernal order of solving an expression
+      // follow the BODMAS RULE
+
+      //questionl
+      //(2+1)*3
+let a =2;
+let b =1;
+let c =3;
+console.log((a+b)*c)
+    //question2
+    //3/1+2**2
+    let q=3;
+    let w=1;
+    let e=2;
+    console.log(q/w+e**e);
+
+    
+
