7150e37455d8132cb51d694ea35090445349f64a
Arsh Mishra <161199109+ArshMishra1@users.noreply.github.com>
2024-07-24 13:56:50 +0530

    Add files via upload

---
+++ Template literals.js
@@ -0,0 +1,13 @@
+let pencilPrice=10;
+let penPrice = 20;  
+//without Teamplate Literals
+
+// console.log("Total pirce is :", pencilPrice + penPrice , " Rupess");
+// let Totalpirce = pencilPrice+penPrice;
+// console.log(Totalpirce);
+
+
+// With Template Literals
+
+console.log(`The total prize : ${pencilPrice+penPrice} Rupepes`);
+
