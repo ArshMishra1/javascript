3d0ba2c492ed6c93893eee992ae3648994bf8206
Arsh Mishra <161199109+ArshMishra1@users.noreply.github.com>
2024-08-01 09:54:07 +0530

    Add files via upload

---
+++ logical operators.js
@@ -0,0 +1,28 @@
+                          // logical operator
+
+// Logical operator are used to combine two or more expression
+// type of logical operators
+//Logical AND 
+//question
+let Mark =30;
+if(Mark >=40 && Mark >=30);{
+    console.log("Good Luck")
+    console.log("A+");
+}
+
+//Logical OR
+// question
+let Number = 10;
+if(Number <= 120 || Number >=100){
+    console.log("OK");
+    
+}
+
+// LOgical NOT 
+// question 
+let num =90;
+if(!(num >=20) && num >=40){
+
+    console.log("YEs");
+
+}
